e704b06e671ff043c07c4adfe0b740704f8689aa
io <io@io.com>
2021-01-20 12:49:52 +0100

    GMaps experiment

--- AppiumUnitTestEmulator.py
+++ AppiumUnitTestEmulator.py
@@ -9,7 +9,7 @@ class AppiumTest(unittest.TestCase):
 
     def setUp(self):
         self.dc['platformName'] = 'Android'
-        self.dc['platformVersion'] = '11'
+        # self.dc['platformVersion'] = '11'
         self.dc['deviceName'] = 'emulator-5554'
         self.dc['appPackage'] = 'com.google.android.apps.maps'
         self.dc['appActivity'] = 'com.google.android.maps.MapsActivity'
@@ -17,7 +17,16 @@ class AppiumTest(unittest.TestCase):
         self.driver = webdriver.Remote("http://0.0.0.0:4723/wd/hub", self.dc)
 
     def testFirstAutomation(self):
-        time.sleep(10)
+        search_box = self.driver.find_element_by_class_name('android.widget.TextView')
+        search_box.click()
+        enter_input_for_search = self.driver.find_element_by_class_name('android.widget.EditText')
+        time.sleep(2)  # to remove asap
+        enter_input_for_search.send_keys("Berlin")
+        berlin_xpath = '//*[contains(@text, "Berlin")] '
+        first_result_to_tap = self.driver.find_element_by_xpath(berlin_xpath)
+        assert first_result_to_tap.text == 'Berlin'
+        first_result_to_tap.click()
+        time.sleep(5)  # just for watching the phone
 
     def tearDown(self):
         self.driver.quit()
